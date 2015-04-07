95e169bd30656be0c59a4fd481e9a3820b095719
winnie1994 <winniepwlin1994@gmail.com>
2015-04-07 00:44:34 -0700

    Update filters.js

--- filters.js
+++ filters.js
@@ -1,7 +1,7 @@
 var imagewidth;
 var imageheight;
 
-//starter
+//starter 
 function filterStarter(){
 	for(var r = 0; r < imagewidth; r++){
 		for(var c = 0; c < imageheight; c++){
@@ -18,12 +18,16 @@ function filterStarter(){
 	}
 }
 
-//tint
+/**Tint: If you want a red tint, for example, you might want to adjust the red color channel's intensity. 
+However, how do you want to do this? Do you increase or decrease the channel, and how? 
+There are many different ways to do this, and we encourage you to experiment with different ones.
+Another interesting question is: how do you make an image take on a red tint without adjusting the red color channel?*/
+
 function filterTint(){
 	//TODO: fill in!
 }
 
-//greyscale!
+//Greyscale!
 function filterGreyscale(){
 	//TODO: fill in!
 }
